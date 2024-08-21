cfe152db4c5249880dede98fe11ab45384ecb004
MetLab-HKUST <88359183+MetLab-HKUST@users.noreply.github.com>
2024-08-21 20:40:48 +0800

    Add two more figures of coarsened fields

---
+++ initial_comparison/plot_qv_pert_coarsened.m
@@ -0,0 +1,217 @@
+%%% This script plots the warm bubble case in CM1 and LEX
+%% first row: time = 0.0 min
+qvpcm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000001.nc", "qv") ;
+qvplex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0000.nc", "qv_now");
+qvplex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0000.nc", "qv_now") ;
+
+qvplex1 = movmean(movmean(movmean(qvplex1, 6, 3), 6, 2), 3, 1);
+qvplex1 = qvplex1(2:3:end,4:6:end,4:6:end);
+
+x_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000001.nc", "xh");
+x_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0000.nc", "x");
+x_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0000.nc", "x");
+z_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000001.nc", "zh");
+z_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0000.nc", "z");
+z_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0000.nc", "z");
+
+x_lex1 = x_lex2;
+z_lex1 = z_lex2;
+
+tiledlayout(4, 3, "TileSpacing", "compact")
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(1000.0*qvpcm1(:,120,:,1))', 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+title("CM1 (\Deltax = 100m)")
+text(-5.7, 6, "t = 0.0 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(1000.0*qvplex1(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+title("LEX (\Deltax = 100m, Coarsened)")
+text(-5.7, 6, "t = 0.0 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(1000.0*qvplex2(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+title("LEX (\Deltax = 600m, SMAG)")
+text(-5.7, 6, "t = 0.0 min")
+grid on
+pbaspect([1,1,1])
+
+%% second row: time = 10.0 min
+qvpcm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000041.nc", "qv") ;
+qvplex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0020.nc", "qv_now") ;
+qvplex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0020.nc", "qv_now") ;
+qvplex1 = movmean(movmean(movmean(qvplex1, 6, 3), 6, 2), 3, 1);
+qvplex1 = qvplex1(2:3:end,4:6:end,4:6:end);
+
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(1000.0*qvpcm1(:,120,:,1))', 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 10 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(1000.0*qvplex1(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 10 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(1000.0*qvplex2(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 10 min")
+grid on
+pbaspect([1,1,1])
+
+%% second row: time = 20.0 min
+qvpcm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000081.nc", "qv") ;
+qvplex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0040.nc", "qv_now") ;
+qvplex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0040.nc", "qv_now") ;
+qvplex1 = movmean(movmean(movmean(qvplex1, 6, 3), 6, 2), 3, 1);
+qvplex1 = qvplex1(2:3:end,4:6:end,4:6:end);
+
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(1000.0*qvpcm1(:,120,:,1))', 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 20 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(1000.0*qvplex1(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 20 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(1000.0*qvplex2(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 20 min")
+grid on
+pbaspect([1,1,1])
+
+%% second row: time = 30.0 min
+qvpcm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000121.nc", "qv") ;
+qvplex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0060.nc", "qv_now");
+qvplex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0060.nc", "qv_now") ;
+qvplex1 = movmean(movmean(movmean(qvplex1, 6, 3), 6, 2), 3, 1);
+qvplex1 = qvplex1(2:3:end,4:6:end,4:6:end);
+
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(1000.0*qvpcm1(:,120,:,1))', 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 30 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(1000.0*qvplex1(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 30 min")
+grid on
+pbaspect([1,1,1])
+
+h = nexttile;
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(1000.0*qvplex2(:,20,:,1)), 'LineColor', 'none')
+colormap(flip(centered2("RdBu", 80)))
+clim([0, 2.3])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 6, "t = 30 min")
+grid on
+pbaspect([1,1,1])
+
+cbh = colorbar(h); 
+cbh.Layout.Tile = 'south';
+cbh.Label.String = "q_v (g/kg)";
+
+
---
+++ initial_comparison/plot_theta_pert_coarsened.m
@@ -0,0 +1,223 @@
+%%% This script plots the warm bubble case in CM1 and LEX
+%% first row: time = 0.0 min
+thp_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000001.nc", "thpert");
+thp_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0000.nc", "theta_now") - ...
+    ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_reference_state.nc", "theta0");
+
+thp_lex1 = movmean(movmean(movmean(thp_lex1, 6, 3), 6, 2), 3, 1);
+thp_lex1 = thp_lex1(2:3:end,4:6:end,4:6:end);
+thp_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0000.nc", "theta_now") - ...
+    ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_reference_state.nc", "theta0");
+
+x_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000001.nc", "xh");
+x_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0000.nc", "x");
+x_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0000.nc", "x");
+z_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000001.nc", "zh");
+z_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0000.nc", "z");
+z_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0000.nc", "z");
+
+x_lex1 = x_lex2;
+z_lex1 = z_lex2;
+
+tiledlayout(4, 3, "TileSpacing", "compact")
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(thp_cm1(:,120,:,1))', 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+title("CM1 (\Deltax = 100m)")
+text(-5.7, 0.75, "t = 0.0 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(thp_lex1(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+title("LEX (\Deltax = 100m, Coarsened)")
+text(-5.7, 0.75, "t = 0.0 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(thp_lex2(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+title("LEX (\Deltax = 600m, SMAG)")
+text(-5.7, 0.75, "t = 0.0 min")
+grid on
+pbaspect([1,1,1])
+
+%% second row: time = 10.0 min
+thp_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000041.nc", "thpert");
+thp_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0020.nc", "theta_now") - ...
+    ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_reference_state.nc", "theta0");
+thp_lex1 = movmean(movmean(movmean(thp_lex1, 6, 3), 6, 2), 3, 1);
+thp_lex1 = thp_lex1(2:3:end,4:6:end,4:6:end);
+thp_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0020.nc", "theta_now") - ...
+    ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_reference_state.nc", "theta0");
+
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(thp_cm1(:,120,:,1))', 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 10 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(thp_lex1(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 10 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(thp_lex2(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 10 min")
+grid on
+pbaspect([1,1,1])
+
+%% second row: time = 20.0 min
+thp_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000081.nc", "thpert");
+thp_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0040.nc", "theta_now") - ...
+    ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_reference_state.nc", "theta0");
+thp_lex1 = movmean(movmean(movmean(thp_lex1, 6, 3), 6, 2), 3, 1);
+thp_lex1 = thp_lex1(2:3:end,4:6:end,4:6:end);
+thp_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0040.nc", "theta_now") - ...
+    ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_reference_state.nc", "theta0");
+
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(thp_cm1(:,120,:,1))', 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 20 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(thp_lex1(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 20 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(thp_lex2(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 20 min")
+grid on
+pbaspect([1,1,1])
+
+%% second row: time = 30.0 min
+thp_cm1 = ncread("CM1_dx100dz100_no_noise\cm1out_000121.nc", "thpert");
+thp_lex1 = ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_out_0060.nc", "theta_now") - ...
+    ncread("LEX_dx100dz100_no_noise_SSPRK3dt10\lex_reference_state.nc", "theta0");
+thp_lex1 = movmean(movmean(movmean(thp_lex1, 6, 3), 6, 2), 3, 1);
+thp_lex1 = thp_lex1(2:3:end,4:6:end,4:6:end);
+thp_lex2 = ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_out_0060.nc", "theta_now") - ...
+    ncread("LEX_dx600dz300_no_noise_SSPRK3dt30_Smag\lex_reference_state.nc", "theta0");
+
+nexttile
+[X, Y] = meshgrid(x_cm1, z_cm1);
+contourf(X, Y, squeeze(thp_cm1(:,120,:,1))', 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 30 min")
+grid on
+pbaspect([1,1,1])
+
+nexttile
+[X, Y] = meshgrid(x_lex1/1000-12.0, z_lex1/1000);
+contourf(X, Y, squeeze(thp_lex1(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 30 min")
+grid on
+pbaspect([1,1,1])
+
+h = nexttile;
+[X, Y] = meshgrid(x_lex2/1000-12.0, z_lex2/1000);
+contourf(X, Y, squeeze(thp_lex2(:,20,:,1)), 'LineColor', 'none')
+colormap(centered("RdBu", 40))
+clim([0, 1])
+xlim([-6, 6])
+xticks([-6, -3, 0, 3, 6])
+ylim([0.15, 12])
+xlabel("x (km)")
+ylabel("z (km)")
+text(-5.7, 0.75, "t = 30 min")
+grid on
+pbaspect([1,1,1])
+
+cbh = colorbar(h); 
+cbh.Layout.Tile = 'south';
+cbh.Label.String = "\theta' (K)";
