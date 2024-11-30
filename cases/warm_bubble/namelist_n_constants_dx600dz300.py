d6b620fd918fc59df578d9d723f048b888c33c58
MetLab-HKUST <88359183+MetLab-HKUST@users.noreply.github.com>
2024-11-30 22:00:01 +0800

    Turn on damping by default

--- cases/warm_bubble/namelist_n_constants_dx600dz300.py
+++ cases/warm_bubble/namelist_n_constants_dx600dz300.py
@@ -1,9 +1,9 @@
 """ Namelist variables and constants for LEX """
 
 # Time integration
-dt = 30            # one-step integration time step
+dt = 15            # one-step integration time step
 sprint_n = 1       # dt*sprint_n is the interval of data saving
-relay_n = 60       # number of sprints, relay_n*sprint_n*dt is the total integration time
+relay_n = 120      # number of sprints, relay_n*sprint_n*dt is the total integration time
 asselin_r = 0.25   # r factor in the Asselin filtering
 integrate_opt = 1  # 1: SSPRK3, 2: Leapfrog
 
@@ -43,7 +43,7 @@ Karman = 0.4    # von Karman constant
 turb_opt = 0    # 1: Smagorinsky
 
 # Rayleigh damping
-damp_opt = False
+damp_opt = True
 z_damping = 9600.0    # height above which Rayleigh damping is applied
 rd_alpha = 1.0/300.0  # Inverse e-folding time for upper - level Rayleigh damping layer
 
